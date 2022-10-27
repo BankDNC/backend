fa35dc63ecb7561c089ed4b776b4166586eaaec4
caobandoc <obandocarlosandres@gmail.com>
2022-10-27 16:00:34 -0500

    correction endpoints

--- src/main/java/com/dnc/bank/controllers/StatusController.java
+++ src/main/java/com/dnc/bank/controllers/StatusController.java
@@ -3,6 +3,7 @@ package com.dnc.bank.controllers;
 import com.dnc.bank.models.response.StatusResponse;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +16,16 @@ public class StatusController {
     @ApiOperation(value = "Get status of the application",
             response = StatusResponse.class)
     @ApiResponse(code = 200, message = "Successfully retrieved status")
-    public StatusResponse statusBase() {
-        return getOk();
+    public ResponseEntity<StatusResponse> statusBase() {
+        return ResponseEntity.ok(getOk());
     }
 
     @GetMapping("/status")
     @ApiOperation(value = "Get status of the application",
             response = StatusResponse.class)
     @ApiResponse(code = 200, message = "Successfully retrieved status")
-    public StatusResponse status() {
-        return getOk();
+    public ResponseEntity<StatusResponse> status() {
+        return ResponseEntity.ok(getOk());
     }
 
     private static StatusResponse getOk() {
