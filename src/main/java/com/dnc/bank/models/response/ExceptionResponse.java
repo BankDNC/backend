8707d589458e5e3d0a60ff6312ea459c3bdcfdc1
caobandoc <obandocarlosandres@gmail.com>
2022-11-10 15:13:55 -0500

    mejora mensajes de error

--- src/main/java/com/dnc/bank/models/response/ExceptionResponse.java
+++ src/main/java/com/dnc/bank/models/response/ExceptionResponse.java
@@ -1,22 +1,16 @@
 package com.dnc.bank.models.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Date;
 
 @Getter
+@Builder
 public class ExceptionResponse {
     private Date timestamp;
     private String message;
     private String path;
     private String httpCodeMessage;
     private Integer httpCode;
-
-    public ExceptionResponse(Date timestamp, String message, String path,String httpCodeMessage, Integer httpCode) {
-        this.timestamp = timestamp;
-        this.message = message;
-        this.path = path;
-        this.httpCodeMessage=httpCodeMessage;
-        this.httpCode=httpCode;
-    }
 }
